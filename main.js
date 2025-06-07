9c32ca70dc2157621633f3e7e8b883bb0ec96455
rayqueoo <133293522+rayqueoo@users.noreply.github.com>
2025-06-07 18:13:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+document.getElementById('app').innerHTML = `
+  <h1 style="color: gold; text-align: center;">Hash Block.IA</h1>
+  <p style="text-align: center;">Plataforma de mineração descentralizada com IA integrada.</p>
+  <ul>
+    <li><a href="login.html">Login</a></li>
+    <li><a href="register.html">Cadastro</a></li>
+    <li><a href="dashboard.html">Dashboard</a></li>
+    <li><a href="withdraw.html">Saque</a></li>
+  </ul>
+`;
